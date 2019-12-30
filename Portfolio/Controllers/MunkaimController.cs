ed0583a689b3e2e8f939e4374f0c29f756487fe6
Dávid Tallósi <tallosi.david69@gmail.com>
2019-12-30 17:10:19 +0100

    Lapok létrehozása és videó lejátszása

---
+++ Portfolio/Controllers/MunkaimController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class MunkaimController : Controller
+    {
+        // GET: Munkaim
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Munkaim/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Munkaim/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Munkaim/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Munkaim/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Munkaim/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Munkaim/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Munkaim/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Portfolio/Models/Munkaim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class Munkaim
+    {
+        public int Id { get; set; }
+        public string Cim { get; set; }
+        public string Kep { get; set; }
+        public string Leiras { get; set; }
+        public DateTime HozzaadasDatuma { get; set; }
+
+    }
+}
