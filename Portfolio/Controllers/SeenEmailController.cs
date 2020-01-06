e26d5b70fa67e0b8742517a5d4c92e0002482821
Dávid Tallósi <tallosi.david69@gmail.com>
2020-01-06 23:08:24 +0100

    CSS fix, email management

--- Portfolio/Controllers/SeenEmailController.cs
+++ Portfolio/Controllers/SeenEmailController.cs
@@ -7,24 +7,15 @@ using Portfolio.Models;
 
 namespace Portfolio.Controllers
 {
-    public class ManageEmailsController : Controller
+    public class SeenEmailController : Controller
     {
         readonly ApplicationDbContext _context;
-        public ManageEmailsController() => _context = new ApplicationDbContext();
-        // GET: ManageEmails
+        public SeenEmailController() => _context = new ApplicationDbContext();
+        // GET: SeenEmail
         public ActionResult Index()
         {
-            var email = _context.Email.Where(f => f.Lattamozott == false).ToList().OrderByDescending(s => s.Id);
+            var email = _context.Email.Where(f => f.Lattamozott == true).ToList().OrderByDescending(s => s.Id);
             return View(email);
-        }
-
-        public ActionResult Seen(int id)
-        {
-            var email = _context.Email.SingleOrDefault(u => u.Id == id);
-            if (email == null) return HttpNotFound();
-            email.Lattamozott = true;
-            _context.SaveChanges();
-            return RedirectToAction("Index", "ManageFeedbacks");
         }
         public ActionResult Delete(int id)
         {
