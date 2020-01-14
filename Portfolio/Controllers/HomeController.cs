486babd89d150e0480fa441df24275f0ba6b7189
Dávid Tallósi <tallosi.david69@gmail.com>
2020-01-14 17:44:00 +0100

    Main Page css

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Portfolio.Models;
 
 namespace Portfolio.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        readonly ApplicationDbContext _context;
+        public HomeController() => _context = new ApplicationDbContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+
+
+            var feedback = _context.Feedback.ToList();
+            var work = _context.Munkaim.ToList();
+
+
+
+
+            SumViewModel model = new SumViewModel();
+            model.Feedbacks = feedback;
+            model.Munkaims = work;
+            return View(model);
         }
     }
 }
---
+++ Portfolio/Models/SumViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class SumViewModel
+    {
+        public virtual IEnumerable<Feedback> Feedbacks { get; set; }
+        public virtual IEnumerable<Munkaim> Munkaims { get; set; }
+    }
+}
